4a955723d4c4e5af1bb1fc474eccb140121a6ab0
Arielle Foldoe <afoldoe@gmail.com>
2016-06-12 13:10:58 -0700

    working on github API functionality

---
+++ scripts/githubToken.js
@@ -0,0 +1,1 @@
+var githubToken = 'b7f10eedfc9f7bff7d577e302d6abb6a5919a5ab';
--- scripts/repos.js
+++ scripts/repos.js
@@ -4,7 +4,27 @@
   repos.all = [];
 
   repos.requestRepos = function() {
-
+    $.ajax({
+      url: 'https://api.github.com/user/repos',
+      type: 'GET',
+      headers: {
+        'Authorizaton' : 'token' + githubToken
+      }
+    })
+    .done(function(data, message, xhr) {
+      repos.all = data;
+    })
+    .fail(function(data, message, xhr) {
+      console.log('noooooooope!');
+    })
+    .then(callback);
   };
 
+  repos.with = function(attr) {
+    return repos.all.filter(function(repo) {
+      return repo[attr];
+    });
+  };
+
+  module.repos = repos;
 })(window);
